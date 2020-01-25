57cc08a0728616cf5952fe7ee4bb3ee7c51b4c97
Rodrigo Silva <obrido@gmail.com>
2020-01-25 15:16:31 -0500

    Error_save and Error delete implemented, new bug found.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Empty from "components/Appointment/Empty";
 import Form from "components/Appointment/Form";
 import Status from "components/Appointment/Status"
 import useVisualMode from "hooks/useVisualMode";
+import Error from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
@@ -49,6 +50,8 @@ const SAVING = "SAVING";
 const DELETE = "DELETE";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE ="ERROR_DELETE";
 
 export default function Appointment(props){
 
@@ -65,7 +68,7 @@ export default function Appointment(props){
     props
       .bookInterview(props.id, interview)
       .then(() => transition(SHOW))
-      // .catch(error => transition(ERROR_SAVE, true));
+      .catch(error => transition(ERROR_SAVE, true));
   }
 
   return <article className="appointment">
@@ -108,6 +111,18 @@ export default function Appointment(props){
       student={props.interview.student}
       interviewer={props.interview.interviewer.id}
      />
+    }
+    {mode  === ERROR_SAVE &&
+    <Error
+      message={`Appointment not saved`}
+      onClose={() => back()}
+      />
+    }
+    {mode === ERROR_DELETE &&
+    <Error
+    message={`Error deleting appointment`}
+      onClose={() => back()}
+    />
 
     }
   </article>
